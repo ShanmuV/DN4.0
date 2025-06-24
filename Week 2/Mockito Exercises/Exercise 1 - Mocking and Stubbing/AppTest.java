4bb4ce2cc455399035eb5628e0b120c323b357a4
Shanmu V <155247847+ShanmuV@users.noreply.github.com>
2025-06-24 15:04:42 +0530

    Done with the first exercise of Mockito

---
+++ Week 2/Mockito Exercises/Exercise 1 - Mocking and Stubbing/AppTest.java
@@ -0,0 +1,32 @@
+package DigitalNurture;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    ExternalAPI api;
+    App app;
+
+    @BeforeEach
+    public void setup(){
+        api = mock(ExternalAPI.class);
+        when(api.getData()).thenReturn("Some Data");
+
+        app = new App(api);
+    }
+
+    @Test
+    public void testData(){
+        String formattedData = app.getFormattedData();
+
+        assertEquals("Some Formatted Data: Some Data", formattedData);
+    }
+}
