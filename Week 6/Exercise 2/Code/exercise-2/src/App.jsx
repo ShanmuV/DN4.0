07fa513a5bb457bebdbc2bb30a62f3eb29751293
Shanmu V <155247847+ShanmuV@users.noreply.github.com>
2025-07-26 11:38:12 +0530

    Done with Exercise 2

---
+++ Week 6/Exercise 2/Code/exercise-2/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+      <About />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
---
+++ Week 6/Exercise 2/Code/exercise-2/src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div>
+      <h1> This is The Home Page!</h1>
+    </div>
+  );
+}
+
+export default HomePage;
